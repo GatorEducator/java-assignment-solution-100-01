7c73a72e0af0eca8e4cf3b98a673a03f9b12f8a8
Megan Corletti <corlettim@allegheny.edu>
2019-10-23 20:09:18 -0400

    move plugin out of the buildscript in build.gradle

--- src/main/java/labone/DisplayOutput.java
+++ src/main/java/labone/DisplayOutput.java
@@ -2,24 +2,23 @@ package labone;
 
 import java.util.Date;
 
-/** This class displays four lines of output.
- * The first line displays the name of the programmer and the date. Each one of
- * the next three lines displays a single statement created by the programmer.
+/**
+ * This class displays four lines of output. The first line displays the name of the programmer and
+ * the date. Each one of the next three lines displays a single statement created by the programmer.
  *
  * <p>Bugs: This program has no known bugs.
  *
  * @author Gregory M. Kapfhammer
  */
-
 public class DisplayOutput {
 
-  /** The main entry point for DisplayOutput.
-   *  The source code inside of this method will be executed when the program is
-   *  run in the command-line. The output of this program will appear in the
-   *  terminal window. The println statements make the program display output.
+  /**
+   * The main entry point for DisplayOutput. The source code inside of this method will be executed
+   * when the program is run in the command-line. The output of this program will appear in the
+   * terminal window. The println statements make the program display output.
    *
    * @param args The command-line arguments
-   **/
+   */
   public static void main(String[] args) {
 
     // display the date using the Date constructor
@@ -28,7 +27,5 @@ public class DisplayOutput {
     System.out.println("Hello World.");
     System.out.println("Gradle is Great.");
     System.out.println("Docker is Dynamite.");
-
   }
-
 }
